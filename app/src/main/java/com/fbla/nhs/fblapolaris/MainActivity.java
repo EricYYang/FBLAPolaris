b28cf04f5246951d797619d5ae04de946a13402f
Eric Yang <18Yang692@sasd.net>
2017-11-27 15:40:51 -0600

    uhm yea

--- app/src/main/java/com/fbla/nhs/fblapolaris/MainActivity.java
+++ app/src/main/java/com/fbla/nhs/fblapolaris/MainActivity.java
@@ -15,7 +15,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+//eat my ass
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
