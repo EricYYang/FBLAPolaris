1053e07df4a9afd9e01f716026430cb96d3c36ef
Eric Yang <18Yang692@sasd.net>
2017-11-29 14:37:10 -0600

    stuff

--- app/src/main/java/com/fbla/nhs/fblapolaris/MainActivity.java
+++ app/src/main/java/com/fbla/nhs/fblapolaris/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.MenuInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -56,6 +57,8 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main_drawer, menu);
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
